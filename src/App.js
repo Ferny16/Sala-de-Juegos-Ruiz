7712d7760ca9962df4bffe33cbe072ce122344b4
Jefernee <jefernee50@gmail.com>
2026-02-05 22:51:09 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import { Routes, Route } from "react-router-dom";
+
+// Router
 import AppRouter from "./components/AppRouter";
-//import Home from "./pages/Home";
+
+// Páginas públicas
 import Home2 from "./pages/Home2";
 import Login from "./pages/Login";
-import Dashboard from "./pages/AgregarProductos";
 import Inscripcion from "./pages/Inscripcion";
+import PublicProductsList from "./pages/PublicProductList";
+
+// Dashboard
+import Dashboard from "./pages/AgregarProductos";
+import SalesDashboard from "./pages/SalesDashboard";
 import ProductsList from "./pages/ProductsList";
 import ManageProducts from "./pages/ManageProducts";
-import SalesDashboard from "./pages/SalesDashboard";
-import PublicProductsList from "./pages/PublicProductList";
 import PedidosDashboard from "./pages/PedidosDashboard";
 import ReportesDashboard from "./pages/ReportesDashboard";
-//import { useBackendWarmup } from "./hocks/useBackendWarmup";
+
+// import { useBackendWarmup } from "./hocks/useBackendWarmup";
 
 function App() {
-  //useBackendWarmup();
+  // useBackendWarmup();
+
   return (
     <AppRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        {/* Rutas públicas */}
         <Route path="/" element={<Home2 />} />
         <Route path="/login" element={<Login />} />
         <Route path="/inscripcion" element={<Inscripcion />} />
         <Route path="/productos" element={<PublicProductsList />} />
-        {/* Rutas del Dashboard */}
+
+        {/* Rutas del dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/sales" element={<SalesDashboard />} />
         <Route path="/dashboard/products" element={<ProductsList />} />
-        <Route path="/dashboard/add-product" element={<Dashboard />} />
         <Route path="/dashboard/manage-products" element={<ManageProducts />} />
         <Route path="/dashboard/pedidos" element={<PedidosDashboard />} />
         <Route path="/dashboard/reportes" element={<ReportesDashboard />} />
@@ -37,3 +44,4 @@ function App() {
 }
 
 export default App;
+
